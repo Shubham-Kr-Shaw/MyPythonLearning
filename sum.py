5d99a3a5fc623b8ac1a58a6c6f9e273d07b2f280
Shubham2443 <sks2443.1947@gmail.com>
2021-01-18 14:20:26 +0530

    Adding 2 no using different methods

---
+++ sum.py
@@ -0,0 +1,14 @@
+print("Adding 2 numbers using predefined values.\n")
+num1 = 15
+num2 = 12
+
+sum = num1 + num2
+print("Sum of the {0} and {1} is {2}" .format(num1, num2,sum))
+
+
+print("\nAdding 2 numbers using user input values.\n")
+n1 = input("Enter the 1st no.-")
+n2 = input("Enter the 2nd no,-")
+sum = int(n1) + int(n2)
+
+print("Sum of the {0} and {1} is {2}" .format(n1, n2,sum))
