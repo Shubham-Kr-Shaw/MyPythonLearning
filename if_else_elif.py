5a4b41817cad78ae68f7ad8902e4568789dc6280
Shubham2443 <sks2443.1947@gmail.com>
2021-01-18 08:59:26 +0530

    A program that checks that you are eligiable for driving a car or not.

--- if_else_elif.py
+++ if_else_elif.py
@@ -1,1 +1,15 @@
+# l1 = [1, 2, 3]
+# print("Enter the number to search it in the list:")
+# x = int(input())
+# if x in l1:
+#     print("Yes ", x, " is in the list")
+# else:
+#     print("No ", x, " is not in the list")
+print("Enter your age?", )
+age = int(input())
+if age==18:
+    print("Please try next year")
+elif age>18:
+    print("Congrats you are eligible for applying for Driving Licence")
+else:
     print("Sorry you are under age you are not eligible")
