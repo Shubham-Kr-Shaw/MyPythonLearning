432d628ed95770138bf6e68f526053c1d18d40d2
Shubham2443 <sks2443.1947@gmail.com>
2021-01-19 16:44:30 +0530

    with opens and closes the file as we use to do previously it does the same.

---
+++ IOwith.py
@@ -0,0 +1,9 @@
+#with opens and closes the file as we use to do previously it does the same.
+"""
+f = open("test.txt","rt")
+f.close()
+
+"""
+with open ("test.txt") as f:
+    a = f.read()
+    print(a)
