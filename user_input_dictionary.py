f1c5a12ea81fec17392eb2911ba37fb24a5e477d
Shubham2443 <sks2443.1947@gmail.com>
2021-01-20 12:37:47 +0530

    A sample dictionary with some words and their key pair. # This program uses dictionary and takes input from user and return the meaning of the word from the dictionary.

---
+++ user_input_dictionary.py
@@ -0,0 +1,5 @@
+# This program uses dictionary and takes input from user and return the meaning of the word from the dictionary.
+
+d1 = {"One": "First natural number", "Two": "Second natural number", "Three": "Third natural number", "Four": "Fourth natural number"}
+word = input("Enter the word to search its meaning: ")
+print(d1[word])
