b92545382d9dc62b0adb1638f3ef5e350daf44ff
Shubham2443 <sks2443.1947@gmail.com>
2021-01-20 12:29:53 +0530

    Find the greater number

---
+++ findTheGreater.py
@@ -0,0 +1,9 @@
+a = input("Enter 1st no:")
+b = input("Enter 2nd no:")
+
+if a==b:
+    print(a, "is equal to",b)
+elif a<b:
+    print(b, "is grater then ",a)
+else:
+    print(a, "is grater then ",b)
