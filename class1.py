ae0bce9e05bd021c7f1e029f7fd8533b5c123f54
Shubham2443 <sks2443.1947@gmail.com>
2021-01-28 16:48:45 +0530

    1st progra on oops.

---
+++ class1.py
@@ -0,0 +1,15 @@
+class student:
+    pass
+
+Shubham = student()
+Nikhil = student()
+
+Shubham.name = "Shubham Kumar Shaw"
+Shubham.age = 21
+Shubham.subject = ["Python", "NLP", ]
+
+Nikhil.name = "Nikhil Kumar"
+Nikhil.age = 21
+Nikhil.subject = ["Python", "ML"]
+
+print(f"My name is {Shubham.name} and my friend's name is {Nikhil.name } & we together are studing on {Shubham.subject}")
